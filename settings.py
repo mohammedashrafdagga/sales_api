0d82336293ede43f8b478a1f4ba8e2f8b475718f
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-06-09 15:24:06 +0300

    connection db postgresql database

--- settings.py
+++ settings.py
@@ -4,7 +4,11 @@ import os
 # load evn variable
 load_dotenv()
 
+# get access to env variable 
+db_username = os.environ.get('DB_USERNAME')
+db_password = os.environ.get('DB_PASSWORD')
+db_server = os.environ.get('DB_SERVER')
+db_name = os.environ.get('DB_NAME')
 
-# All Global Variable
-
-# Connect DB
+#  Global Variable
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_username}:{db_password}@{db_server}/{db_name}"
